a2f03b2ffdcf15da391db05c0563e8b04b1ed18f
Ahmed Elaguab <elaguabahmed97@gmail.com>
2020-06-13 15:22:10 +0100

    Add navbar bars toggle functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const headerBars = document.querySelector('.header__bars-btn')
+const headerBarsImg = headerBars.querySelector('img')
 const phoneNavbar = document.querySelector('.navbar-sc-phone')
 headerBars.addEventListener('click', function (e) {
   phoneNavbar.classList.toggle('navbar-sc-phone--open')
+  if (phoneNavbar.classList.contains('navbar-sc-phone--open')) {
+    headerBarsImg.src = './images/icon-close.svg'
+  } else {
+    headerBarsImg.src = './images/icon-hamburger.svg'
+  }
 })
